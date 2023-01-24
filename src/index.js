aed144368f4a9e51d03ad34f2fae58a3723145b2
Maciek Sitkowski <msitkowski94@gmail.com>
2023-01-24 10:40:11 +0100

    init webpack to bundle react using babel

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
